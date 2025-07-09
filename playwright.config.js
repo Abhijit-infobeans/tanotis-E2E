8b42a733bf83bef38a15eba73f4c25546e9d161d
Abhijit-infobeans <abhijit.sonawane@infobeans.com>
2025-07-09 13:04:46 +0530

    updated config.js file

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,19 @@
-
-
 const config = {
     use: {
-      headless: true, 
+      headless: true,
       viewport: { width: 1280, height: 720 },
-      baseURL: 'https://www.tanotis.com', 
+      baseURL: 'https://www.tanotis.com',
       screenshot: 'only-on-failure',
       video: 'retain-on-failure',
     },
     timeout: 30 * 1000,
     testDir: './tests',
     retries: 0,
-    reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
+    reporter: [
+      ['list'],
+      ['html', { outputFolder: 'playwright-report', open: 'never' }],
+      ['allure-playwright'] 
+    ],
   };
   
   module.exports = config;
