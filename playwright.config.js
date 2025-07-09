fbe141fa03e8da4b4ee1cbec03a11ccdea1c7906
Abhijit-infobeans <abhijit.sonawane@infobeans.com>
2025-07-09 12:57:26 +0530

    updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 
 const config = {
     use: {
-      headless: false, 
+      headless: true, 
       viewport: { width: 1280, height: 720 },
       baseURL: 'https://www.tanotis.com', 
       screenshot: 'only-on-failure',
